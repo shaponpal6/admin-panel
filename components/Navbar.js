fd4171718d0b40d112e0cf6ee159b605bb1d5fbb
shapon pal <shaponpal6@gmail.com>
2022-11-20 00:56:47 +0600

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { UserContext } from '@lib/context';
+import { auth } from '@lib/firebase';
+
+// Top navbar
+export default function Navbar() {
+  const { user, username } = useContext(UserContext);
+
+  const router = useRouter();
+
+  const signOut =  () => {
+    auth.signOut();
+    router.reload();
+  }
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">NXT</button>
+          </Link>
+        </li>
+
+        {/* user is signed-in and has username */}
+        {username && (
+          <>
+            {/* <li className="push-left">
+              <button onClick={signOut}>Sign Out</button>
+            </li> */}
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write Posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/quiz">
+                <button className="btn-red">Create QUIZ</button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/course">
+                <button className="btn-green">Create Course</button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/competition">
+                <button className="btn-blue">Competition</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <img src={user?.photoURL || '/hacker.png'} />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is not signed OR has not created username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
---
+++ pages/[username]/index.js
@@ -0,0 +1,55 @@
+import { getUserWithUsername, postToJSON } from '@lib/firebase';
+import UserProfile from '@components/UserProfile';
+import Metatags from '@components/Metatags';
+import PostFeed from '@components/PostFeed';
+import { useRouter } from 'next/router';
+import { auth } from '@lib/firebase';
+
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
+  const router = useRouter();
+
+  const signOut =  () => {
+    auth.signOut();
+    router.reload();
+  }
+  return (
+    <main>
+      <Metatags title={user.username} description={`${user.username}'s public profile`} />
+      <UserProfile user={user} />
+      <button onClick={signOut}>Sign Out</button>
+      <PostFeed posts={posts} />
+    </main>
+  );
+}
